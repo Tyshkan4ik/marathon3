8f358f49144652d8f8d7ad8bc0c880d1f3a241b8
Tyshkan4ik <001vat@gmail.com>
2023-03-09 19:50:30 +0300

    Initial Commit

---
+++ 3 marathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3 marathon
+//
+//  Created by Виталий Троицкий on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
